c161575bc8a3b256c9ac8c89e31c18a8e4016497
yanglele <youngle316@gmail.com>
2020-08-12 23:18:35 +0800

    Updates

---
+++ precache-manifest.3143128ef3fb30e9debeb428648bcfec.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f079ad607d2b3e16672bf3d875e862e",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "d64774617161b974979b",
+    "url": "/covid19/static/css/6.4f3b00d7.chunk.css"
+  },
+  {
+    "revision": "638ec33db0cfb22975de",
+    "url": "/covid19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3bb505eec4629ffbf70f",
+    "url": "/covid19/static/js/0.882914dc.chunk.js"
+  },
+  {
+    "revision": "90dea8c765b9df96e103",
+    "url": "/covid19/static/js/10.d9176967.chunk.js"
+  },
+  {
+    "revision": "9b97205da64c814d2f30",
+    "url": "/covid19/static/js/11.a74ada2d.chunk.js"
+  },
+  {
+    "revision": "025bee9614959909f72f",
+    "url": "/covid19/static/js/3.ab0863cd.chunk.js"
+  },
+  {
+    "revision": "1fa2298c1254c4345de7e1b8d2d7623f",
+    "url": "/covid19/static/js/3.ab0863cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81b83c42a199941b0af",
+    "url": "/covid19/static/js/4.62bbcb9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19/static/js/4.62bbcb9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68e05a1869f04313c2b",
+    "url": "/covid19/static/js/5.f122b592.chunk.js"
+  },
+  {
+    "revision": "d64774617161b974979b",
+    "url": "/covid19/static/js/6.217679e7.chunk.js"
+  },
+  {
+    "revision": "72617b58fe1b279fb692",
+    "url": "/covid19/static/js/7.6caa9a52.chunk.js"
+  },
+  {
+    "revision": "bc1ca3c5a977432d5289",
+    "url": "/covid19/static/js/8.b6c8085d.chunk.js"
+  },
+  {
+    "revision": "849a35554110819128e8",
+    "url": "/covid19/static/js/9.c4ed2449.chunk.js"
+  },
+  {
+    "revision": "638ec33db0cfb22975de",
+    "url": "/covid19/static/js/main.a85d2e1b.chunk.js"
+  },
+  {
+    "revision": "a947d0857201796374fe",
+    "url": "/covid19/static/js/runtime-main.06596cbd.js"
+  }
+]);
